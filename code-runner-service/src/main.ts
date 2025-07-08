56b6ddbd78159029e3b7616264a2e6dd0ffc41a6
emberow <chin45630@gmail.com>
2025-07-08 14:53:33 +0800

    feat: add debug mode

--- code-runner-service/src/main.ts
+++ code-runner-service/src/main.ts
@@ -1,8 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
 dotenv.config();
 
 async function bootstrap() {
