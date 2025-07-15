cfe9ad3e0efb31c15f6509305e1c382e4911133d
emberow <chin45630@gmail.com>
2025-07-15 11:43:05 +0800

    feat: add bootstrap log

--- code-runner-service/src/main.ts
+++ code-runner-service/src/main.ts
@@ -1,13 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT ?? 3001);
+  console.log(
+    `Application is running on: http://localhost:${process.env.PORT ?? 3001}`,
+  );
+  console.log(
+    `Swagger is running on: http://localhost:${process.env.PORT ?? 3001}/api`,
+  );
 }
 
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-bootstrap();
+void bootstrap();
